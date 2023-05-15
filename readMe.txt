bd70fb917cc1ca5f03e5552c82faf2a115259c49
Diegomrb <75950612+Diegomrb@users.noreply.github.com>
2023-05-15 09:10:02 -0300

    cambios en content y readme

---
+++ readMe.txt
@@ -0,0 +1,11 @@
+Para abrir en servidor local, instalamos node modules: npm i
+
+Para hacer cambios en el código y que se guarde en la pagina de Github Pages:
+
+"git status"
+"git add ."
+"git commit -m "cambios realizados"
+"git push"
+
+"npm run deploy"
+Al actualizar la pagina se verán los cambios.
--- src/content_option.js
+++ src/content_option.js
@@ -77,11 +77,25 @@ const services = [{
     },
 ];
 
-const dataportfolio = [{
+const dataportfolio = [
+        {
+        name: "Tfrita",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7a14d7170029075.6461540b61d7d.jpg",
+        description: "Página web interactiva de recetas uruguayas según distintos filtros y funcionalidades.",
+        descripcionAmpliacion: "Página web de recetas de cocina uruguaya, tiene varias características dinámicas como la funcionalidad de mostrar recetas por horario del día. También permite filtrar recetas y buscarlas según las necesidades del usuario, como por ejemplo buscar directo un almuerzo de dificultad fácil de la lista a partir de la seleccion de multiples filtros.",
+        loomVideo: "https://www.loom.com/embed/65f6de3b79ab43a3a3546f9c1299b3df",
+        imgbig: "",
+        captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6ea87170029075.64619cce3dd2d.png",
+        captura2: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/791327170029075.64619cce3f181.png",
+        captura3: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/dab50a170029075.64619cce3e759.png",
+        linkExterno: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/84ebfb170029075.6456fa09a4c58.jpg",
+    },
+    {
+    
         name: "Clima Turístico",
         img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/fb1e66170029075.6461540b5f06b.jpg",
         description: "Elegí una ciudad y enterate del plan perfecto según el clima.",
-        descripcionAmpliacion: "",
+        descripcionAmpliacion: "Esta app o página web te permite elegir un destino Uruguayo y conocer los datos sobre el clima el día de la fecha y además te sugiere lugares que visitar según pronóstico del tiempo. Por ejemplo si el clima está lluvioso, te recomendará algun museo o plan en un lugar resguardado del frio o la lluvia.",
         loomVideo:"https://www.loom.com/embed/236c02a9d7834f1b945504608e653c3b",
         imgbig: "#",
         captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/40ab0d170029075.64616c37491db.png",
@@ -93,7 +107,7 @@ const dataportfolio = [{
         name: "MovieList",
         img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/93e4de170029075.6461540b5e414.jpg",
         description: "Listado de peliculas y series en para mirar.",
-        descripcionAmpliacion: "",
+        descripcionAmpliacion: "La idea de esta app viene de que personalmente tengo como recordatorio muchas películas y series que quiero ver en las notas de mi celular. Porqué no tener una app dedicada con funcionalidades especiales y que faciliten esa acción de agregar películas y series, su categoría, algún comentario o demás detalles de la serie o película en mi lista.",
         loomVideo: "https://www.loom.com/embed/c3b6dc5f0be5470f8a44890323a3972c",
         imgbig: "#",
         captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/8dbce8170029075.64617949d5858.jpg",
@@ -117,7 +131,7 @@ const dataportfolio = [{
         name: "Museo Soles",
         img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2a6687170029075.6461540b6513b.jpg",
         description: "Museo ficticio inspirado en la vida y obra de Carlos Páez Vilaró.",
-        descripcionAmpliacion: "Mis primeros inicios en el diseño web, una página web sobre un museo fictício de Carlos Páez Vilaró, utilizando colores e iconografía tomada de sus própias obras, y agregandole también información biográfica, obras del artista y datos inventados sobre lo que sería el museo si enrealidad existiera.",
+        descripcionAmpliacion: "Soles es una página web sobre un museo fictício de Carlos Páez Vilaró, utilizando colores e iconografía tomada de sus própias obras, y agregandole también información biográfica, obras del artista y datos inventados sobre lo que sería el museo si enrealidad existiera.",
         loomVideo: "https://www.loom.com/embed/6c4a4ab692e74370a233ecb26479c924",
         imgbig: "#",
         captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b7f1db170029075.64617eef6a19a.png",
@@ -125,23 +139,12 @@ const dataportfolio = [{
         captura3: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/6310aa170029075.64617eef694c4.png",
         linkExterno: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/84ebfb170029075.6456fa09a4c58.jpg",
     },
-    {
-        name: "CoinVersor",
-        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/744c04170029075.6461540b626be.jpg",
-        description: "Elegí una moneda y convertila en la criptomoneda que quieras.",
-        descripcionAmpliacion: "Con esta app desarrollada utilizando Javascript Vanilla y la api de CoinPaprika podremos cambiar el monto que seleccionemos de una moneda tradicional a la criptomoneda elegida.",
-        /* loomVideo: "#", */
-        imgbig: "#",
-        captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/98d9e9170029075.646180be368b5.jpg",
-        captura2: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b0a232170029075.646180be3470e.jpg",
-        captura3: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c4e370170029075.646180be35b42.jpg",
-        linkExterno: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/84ebfb170029075.6456fa09a4c58.jpg",
-    },
+   
     {
         name: "DinamotoWeb",
         img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/8c491a170029075.6461540b63c24.jpg",
         description: "Prototipo de la web de la empresa Uruguaya Dinamoto de venta de vehículos eléctricos.",
-        descripcionAmpliacion: "",
+        descripcionAmpliacion: "Trabajo con el fin de realizar el rediseño de la página web de la empresa uruguaya Dinamoto, dedicada a la venta de vehículos eléctricos. Trabajé dos años con esta empresa y me parecía que su página web tenía algunas cosas por mejorar, por eso la utilicé como ejemplo a la hora de elegir el proyecto para la materia de Interfaces Digitales.",
         loomVideo: "https://www.loom.com/embed/2765b3d0debe479395ff6b0fac0e19fa",
         imgbig: "#",
         captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/31482b170029075.646168ec6e810.jpg",
@@ -237,7 +240,7 @@ const dataportfolio = [{
         name: "Idep",
         img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/1ae00b170029075.6461540b5fd25.jpg",
         description: "App de compra y reserva de pasajes de viajes interdepartamentales.",
-        descripcionAmpliacion: "",
+        descripcionAmpliacion: "Al ser un estudiante del interior que tuvo que mudarse a Montevideo para realizar sus estudios universitarios, siempre tuve que hacer uso del transporte interdepartamental. Como son servicios que tienen muchos años a veces están poco actualizados y la idea de esta app es poder hacer todo lo que hago en la terminal desde cualquier lugar. Marcar pasajes, resevar. Coordinar un viaje completo por la empresa elegida dependiendo del origen destino y la fecha desde mi celular. Y que no sea necesario ir hasta un lugar físico.",
         loomVideo: "https://www.loom.com/embed/62fba1a6ddab484cbe794ffae4a3dbf3",
         imgbig: "#",
         captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/de5585170029075.64619a5123a75.jpg",
@@ -245,18 +248,7 @@ const dataportfolio = [{
         captura3: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/b7f962170029075.64619a51220fd.png",
         linkExterno: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/84ebfb170029075.6456fa09a4c58.jpg",
     },
-    {
-        name: "Tfrita",
-        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7a14d7170029075.6461540b61d7d.jpg",
-        description: "Página web interactiva de recetas uruguayas según distintos filtros y funcionalidades.",
-        descripcionAmpliacion: "Página web de recetas de cocina uruguaya, tiene varias características dinámicas como la funcionalidad de mostrar recetas por horario del día. También permite filtrar recetas y buscarlas según las necesidades del usuario, como por ejemplo buscar directo un almuerzo de dificultad fácil de la lista a partir de la seleccion de multiples filtros.",
-        loomVideo: "https://www.loom.com/embed/65f6de3b79ab43a3a3546f9c1299b3df",
-        imgbig: "",
-        captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6ea87170029075.64619cce3dd2d.png",
-        captura2: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/791327170029075.64619cce3f181.png",
-        captura3: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/dab50a170029075.64619cce3e759.png",
-        linkExterno: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/84ebfb170029075.6456fa09a4c58.jpg",
-    },
+
     {
         name: "Puesta en página",
         img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/adadbf170029075.6461540b60725.jpg",
@@ -268,6 +260,18 @@ const dataportfolio = [{
         captura2: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/6e85ca170029075.64619d286c9e4.png",
         captura3: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/176092170029075.64619d286ad58.png",
         linkExterno: "https://www.behance.net/gallery/123595583/Revista-Turistica-Argentina/modules/702474865",
+    },
+     {
+        name: "CoinVersor",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/744c04170029075.6461540b626be.jpg",
+        description: "Elegí una moneda y convertila en la criptomoneda que quieras.",
+        descripcionAmpliacion: "Con esta app desarrollada utilizando Javascript Vanilla y la api de CoinPaprika podremos cambiar el monto que seleccionemos de una moneda tradicional a la criptomoneda elegida.",
+        /* loomVideo: "#", */
+        imgbig: "#",
+        captura1: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/98d9e9170029075.646180be368b5.jpg",
+        captura2: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b0a232170029075.646180be3470e.jpg",
+        captura3: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c4e370170029075.646180be35b42.jpg",
+        linkExterno: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/84ebfb170029075.6456fa09a4c58.jpg",
     },
 ];
 
